765544ea540a1e74d3ed15b1b08e56351416f936
HullHound <HullHound@hotmail.com>
2024-02-09 08:06:47 +0000

    Updated Project to Godot 4

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 A Godot starter project for the nokia3310 game jam
 
 Use this project to get a kick start for the Nokia 3310 Jam on itch.io!
-[Link to the Jam](https://itch.io/jam/nokiajam3)
+[Link to the Jam](https://itch.io/jam/nokiajam6)
 
 
-Everything inside the Game.tscn scene will be rendered with two colors from the official palette while also applying an ordered dithering shader.  
+Everything inside the Game.tscn scene will be rendered with two colors from the official palette while also applying an ordered dithering shader.
+
+## Recommendations for best results
+1. Load images in LossLess or VRAM Uncompressed Modes
+2. If using 2D, only use whole pixels (not fractional pixels (ie. Having something positioned at 1.5, 1.5))
 
 ![Example screen](/example.png)
--- data/scenes/Sprite.gd
+++ data/scenes/Sprite.gd
@@ -1,8 +1,11 @@
-extends Sprite
+extends Sprite2D
 
+@export var round_to_pixel: bool = false;
 
 var sfx:AudioStream = preload("res://data/sfx/sfx01.wav")
 var _original_position:Vector2 = Vector2()
+var _accurate_position: Vector2 = Vector2()
+
 var _position:Vector2 = Vector2()
 var _velocity:Vector2 = Vector2()
 
@@ -10,14 +13,19 @@ var _velocity:Vector2 = Vector2()
 func _ready() -> void:
 	_original_position = position
 
-
 func _process(delta: float) -> void:
 	_velocity.y -= 60.0 * delta
 	_position += _velocity * delta
 	if _position.y < 0:
 		_velocity.y -= _position.y
 		_position.y = 0.0
-	position = _original_position - _position
+		
+	_accurate_position =  _original_position - _position
+		
+	if round_to_pixel:
+		position = Vector2i(_accurate_position)
+	else:
+		position = _accurate_position
 
 
 func _input(event: InputEvent) -> void:
